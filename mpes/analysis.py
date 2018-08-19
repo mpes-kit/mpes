d66050eba366aee1d2007ed8cdfa3be5e9483e7c
RealPolitiX <xrpatrick@gmail.com>
2018-08-19 15:25:13 +0200

    Added mpes.pointops.order_pointset()

--- mpes/analysis.py
+++ mpes/analysis.py
@@ -303,6 +303,7 @@ def _datacheck_peakdetect(x_axis, y_axis):
     # Needs to be a numpy array
     y_axis = np.array(y_axis)
     x_axis = np.array(x_axis)
+
     return x_axis, y_axis
 
 
@@ -349,9 +350,10 @@ def peakdetect1d(y_axis, x_axis = None, lookahead = 200, delta=0):
     length = len(y_axis)
 
 
-    #perform some checks
+    # Perform some checks
     if lookahead < 1:
         raise ValueError("Lookahead must be '1' or above in value")
+
     if not (np.isscalar(delta) and delta >= 0):
         raise ValueError("delta must be a positive number")
 
@@ -359,12 +361,13 @@ def peakdetect1d(y_axis, x_axis = None, lookahead = 200, delta=0):
     #mx and mn respectively
     mn, mx = np.Inf, -np.Inf
 
-    #Only detect peak if there is 'lookahead' amount of points after it
+    # Only detect peak if there is 'lookahead' amount of points after it
     for index, (x, y) in enumerate(zip(x_axis[:-lookahead],
                                         y_axis[:-lookahead])):
         if y > mx:
             mx = y
             mxpos = x
+
         if y < mn:
             mn = y
             mnpos = x
@@ -374,11 +377,13 @@ def peakdetect1d(y_axis, x_axis = None, lookahead = 200, delta=0):
             # Maxima peak candidate found
             # look ahead in signal to ensure that this is a peak and not jitter
             if y_axis[index:index+lookahead].max() < mx:
+
                 max_peaks.append([mxpos, mx])
                 dump.append(True)
                 # Set algorithm to only find minima now
                 mx = np.Inf
                 mn = np.Inf
+
                 if index+lookahead >= length:
                     # The end is within lookahead no more peaks can be found
                     break
@@ -392,11 +397,13 @@ def peakdetect1d(y_axis, x_axis = None, lookahead = 200, delta=0):
             # Minima peak candidate found
             # look ahead in signal to ensure that this is a peak and not jitter
             if y_axis[index:index+lookahead].min() > mn:
+
                 min_peaks.append([mnpos, mn])
                 dump.append(False)
                 # Set algorithm to only find maxima now
                 mn = -np.Inf
                 mx = -np.Inf
+
                 if index+lookahead >= length:
                     # The end is within lookahead no more peaks can be found
                     break
@@ -405,13 +412,14 @@ def peakdetect1d(y_axis, x_axis = None, lookahead = 200, delta=0):
             #    mnpos = x_axis[np.where(y_axis[index:index+lookahead]==mn)]
 
 
-    #Remove the false hit on the first value of the y_axis
+    # Remove the false hit on the first value of the y_axis
     try:
         if dump[0]:
             max_peaks.pop(0)
         else:
             min_peaks.pop(0)
         del dump
+
     except IndexError: # When no peaks have been found
         pass
 
--- mpes/pointops.py
+++ mpes/pointops.py
@@ -13,35 +13,68 @@ import numpy as np
 from numpy.linalg import norm
 
 
-def pointset_center(pset, condition='among', method='medcp'):
+def pointset_center(pset, condition='among', method='meancp'):
     """
     Determine the center position of a point set
 
     :Parameters:
         pset : 2D array
             Pixel coordinates of the point set.
-        condition : str
+        condition : str | 'among'
             Condition to extract the points
             'among' = use a point among the set
-            'unrestricted' = use the median coordinate
-        method : str
+            'unrestricted' = use the centroid coordinate
+        method : str | 'meancp'
             Method to determine the point set center.
     """
 
-    med = np.median(pset[:, 0]), np.median(pset[:, 1])
+    # Centroid position of point set
+    pmean = np.mean(pset, axis=0)
 
-    if method == 'medcp':
-        dist = norm(pset - med, axis=1)
-        pscenter = pset[np.argmin(dist), :]
+    # Compare the coordinates with the mean position
+    if method == 'meancp':
+        dist = norm(pset - pmean, axis=1)
+        minid = np.argmin(dist)
+        pscenter = pset[minid, :]
+        prest = np.delete(pset, minid, axis=0)
     else:
         raise NotImplementedError
 
     if condition == 'among':
-        return pscenter
+        return pscenter, prest
     elif condition == 'unrestricted':
-        return med
+        return pmean
 
 
-def order_polygon(pset, direction):
+def order_pointset(pset, center=None, direction='cw'):
+    """
+    Order a point set around a centroid in a clockwise or counterclockwise way.
 
-    return
+    :Parameters:
+        pset : 2D array
+            Pixel coordinates of the point set.
+        center : list/tuple/1D array | None
+            Pixel coordinates of the putative shape center.
+        direction : str | 'cw'
+            Direction of the ordering ('cw' or 'ccw').
+
+    :Return:
+        pset_ordered : 2D array
+            Sorted pixel coordinates of the point set.
+    """
+
+    dirdict = {'cw':1, 'ccw':-1}
+
+    # Calculate the coordinates of the
+    if center is None:
+        pmean = np.mean(pset, axis=0)
+        pshifted = pset - pmean
+    else:
+        pshifted = pset - center
+
+    pangle = np.arctan2(pshifted[:, 1], pshifted[:, 0]) * 180/np.pi
+    # Sorting order
+    order = np.argsort(pangle)[::dirdict[direction]]
+    pset_ordered = pset[order]
+
+    return pset_ordered
