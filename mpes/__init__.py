3fbd309904657455958e859041f51a7a1bcd4012
RealPolitiX <xrpatrick@gmail.com>
2020-07-28 12:09:31 -0500

    Updated the version to 1.0.3

--- mpes/__init__.py
+++ mpes/__init__.py
@@ -1,6 +1,6 @@
 from . import utils
 
-__version_info__ = ('0', '9', '8')
+__version_info__ = ('1', '0', '3')
 __version__ = '.'.join(__version_info__)
 __author__ = 'R. Patrick Xian, Laurenz Rettig'
 __all__ = ['fprocessing', 'analysis', 'visualization', 'utils']
