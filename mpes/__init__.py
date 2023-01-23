adeacb4eacd4dfff0f2c72d6accc73a1d0d89cf5
rettigl <l.rettig@web.de>
2023-01-23 20:49:08 +0100

    bump version to 1.1.4

--- mpes/__init__.py
+++ mpes/__init__.py
@@ -1,6 +1,6 @@
 from . import utils
 
-__version_info__ = ('1', '1', '2')
+__version_info__ = ('1', '1', '4')
 __version__ = '.'.join(__version_info__)
-__author__ = 'R. Patrick Xian, Laurenz Rettig'
+__author__ = 'R. Patrick Xian, Laurenz Rettig, Abeer Arora'
 __all__ = ['fprocessing', 'analysis', 'visualization', 'utils', 'mirrorutil']
